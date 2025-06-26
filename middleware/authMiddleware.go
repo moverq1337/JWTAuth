ff59b27b9b791295e3a1196133c274973cbc9c42
moverq <moverq1337>
2025-06-26 15:06:10 +0300

    authMiddleware update

--- middleware/authMiddleware.go
+++ middleware/authMiddleware.go
@@ -4,30 +4,27 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/moverq1337/JWTAuth/utils"
 	"log"
+	"strings"
 )
 
-type TokenRequest struct {
-	Token string `json:"token"`
-}
-
 func AuthMiddleware(c *gin.Context) {
-	var req TokenRequest
-	if err := c.ShouldBindJSON(&req); err != nil {
+	res := c.GetHeader("Authorization")
+	tokenString := strings.Split(res, " ")
+	if tokenString[0] != "Bearer" {
 		c.JSON(400, gin.H{
-			"error": "token is not defined",
+			"error": "token is incorrect type",
 		})
-		log.Println(err)
 		c.Abort()
-
+		c.Abort()
 	}
-	if req.Token == "" {
+	token := tokenString[1]
+	if token == "" {
 		c.JSON(400, gin.H{
 			"error": "token is null",
 		})
 		c.Abort()
-
 	}
-	if err := utils.VerifyToken(req.Token); err != nil {
+	if err := utils.VerifyToken(token); err != nil {
 		c.JSON(400, gin.H{
 			"error": "token is not valid",
 		})
